3dee3c74b65dacbd5e0cfb8187cbc4033b6da44b
James Halliday <mail@substack.net>
2012-08-20 23:59:09 -0700

    drop down some browsers

--- badge.js
+++ badge.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 var Canvas = require('canvas');
 var canvas = new Canvas(262, 120);
 var ctx = canvas.getContext('2d');
@@ -27,6 +29,18 @@ ctx.fillStyle = 'rgb(31,31,31)';
 round(2, 2, 262 - 4, 120 - 4, 20);
 ctx.fill();
 
+var browsers = [ 'explorer', 'chrome', 'safari', 'firefox', 'opera' ];
+browsers.forEach(function (browser, ix) {
+    var file = __dirname + '/static/' + browser + '.png';
+    var data = fs.readFileSync(file, 'base64');
+    var img = new Canvas.Image;
+    img.src = 'data:image/png;base64,' + data;
+    
+    var x = 5 + 51 * ix + (51 - img.width * 0.6) / 2;
+    var w = img.width * 0.6;
+    var h = img.height * 0.6;
+    ctx.drawImage(img, x, 10, w, h);
+});
+
 var s = canvas.createPNGStream();
-var fs = require('fs');
 s.pipe(fs.createWriteStream('round.png'));
