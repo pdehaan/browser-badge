b205fa21c1b9ac300ad0837369a08d9a8713e72e
James Halliday <mail@substack.net>
2012-08-20 22:38:09 -0700

    inset rounded rects

--- badge.js
+++ badge.js
@@ -2,27 +2,31 @@ var Canvas = require('canvas');
 var canvas = new Canvas(262, 120);
 var ctx = canvas.getContext('2d');
 
-ctx.lineWidth = 4;
-ctx.strokeStyle = 'green';
-
-var w = 262, h = 120, r = 40;
-
-var x = 0, y = 0;
-ctx.moveTo(x, y + r);
-
-for (var angle = 0; angle <= 2 * Math.PI; angle += Math.PI / 2) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
+function round (x, y, w, h, r) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + r);
     
-    ctx.arcTo(x, y, x + w * c, y + h * s, r);
-    ctx.lineTo(x + c * r, y + s * r);
-    
-    x += c * w;
-    y += s * h;
+    for (var angle = 0; angle <= 2 * Math.PI; angle += Math.PI / 2) {
+        var c = Math.cos(angle);
+        var s = Math.sin(angle);
+        
+        ctx.arcTo(x, y, x + w * c, y + h * s, r);
+        ctx.lineTo(x + c * r, y + s * r);
+        
+        x += c * w;
+        y += s * h;
+    }
+    ctx.closePath();
 }
+ 
+ctx.fillStyle = 'rgb(191,191,191)';
+round(0, 0, 262, 120, 20);
+ctx.fill();
 
-ctx.stroke();
+ctx.fillStyle = 'rgb(31,31,31)';
+round(2, 2, 262 - 4, 120 - 4, 20);
+ctx.fill();
 
 var s = canvas.createPNGStream();
 var fs = require('fs');
-s.pipe(fs.createWriteStream('badge.png'));
+s.pipe(fs.createWriteStream('round.png'));
